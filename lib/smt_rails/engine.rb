6e4377b0af1e099343e4d6d15b566e43aae5c65f
Alexey <leopard.not.a@gmail.com>
2012-04-25 15:04:48 +0300

    fix engine

--- lib/smt_rails/engine.rb
+++ lib/smt_rails/engine.rb
@@ -1,9 +1,13 @@
 module SmtRails
   class Engine < ::Rails::Engine
+    config.before_configuration do |app|
+      app.paths['app/views'] << Rails.root.join("app", "templates") 
+    end
+    
     initializer "sprockets.smt_rails", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
       app.assets.register_engine(".#{SmtRails.template_extension}", Tilt)
-      app.config.assets.paths << Rails.root.join("app", "views")
+      app.config.assets.paths << Rails.root.join("app", "templates")
     end
   end
 end
